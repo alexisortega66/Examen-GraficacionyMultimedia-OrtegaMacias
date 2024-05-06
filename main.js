3d8bbea6a5655a196047905e5fd6bc6773603ce6
alexiss <alexisortega66>
2024-05-06 15:57:52 -0700

    Intrucciones de examen

---
+++ main.js
@@ -0,0 +1,24 @@
+//Examen de graficacion y multimedia
+/*
+intrucciones de examen
+Crear una aplicación en p5 la cual dibujará 3 relojes análogos. Cada reloj tendrá una hora de acuerdo a las siguientes ciudades. 
+
+Reloj 1: La Paz, BCS
+
+Reloj 2: Ciudad de México
+
+Reloj 3: Barcelona, Esp
+
+Se deberá contar con un input(time) que permita configurar la hora del primer reloj(La Paz, BCS), mientras que los otros reloj se ajustarán tomando en cuenta la diferencia de hora con el primer reloj.
+
+Consideraciones:
+
+El Segundero se deberá mover cada segundo.
+El minutero se deberá mover cada minuto
+El Horario se deberá mover cada hora
+Para dibujar la circunferencia de cada uno de los relojes deberán utilizar el algoritmo Punto y medio para círculos
+Para las manecillas del primer reloj se realizarán utilizando el algoritmo de la ecuación punto-pendiente
+Para las manecillas del segundo reloj se realizarán utilizando el algoritmo DDA
+Para las manecillas del tercer reloj se realizarán utilizando el algoritmo BRESENHAM
+
+*/
